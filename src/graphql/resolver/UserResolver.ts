e301116095a529e433e44b6d4f1389acc486bf49
OmarSalem123 <108749348+OmarSalem123@users.noreply.github.com>
2024-06-25 21:36:11 +0300

    data updated

--- src/graphql/resolver/UserResolver.ts
+++ src/graphql/resolver/UserResolver.ts
@@ -1,14 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
 import { UserType } from '../models/User';
+import { Users } from 'src/_data_/data';
 
 @Resolver()
 export class UserResolver {
   @Query((returns) => UserType)
-  getUser() {
-    return {
-      userId: 1,
-      firstName: 'omar',
-      fatherName: 'aly',
-    };
+  getUserById(@Args('userId', { type: () => Int }) userId: number) {
+    return Users.find(user => user.userId === userId);
   }
 }
