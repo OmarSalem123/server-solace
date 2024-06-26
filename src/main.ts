6a875cfd5df483fff6da07660a5ea7d6c09c272f
OmarSalem123 <108749348+OmarSalem123@users.noreply.github.com>
2024-06-26 13:05:27 +0300

    server done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS with specific options
+  app.use(
+    cors({
+      origin: 'http://localhost:3000', // Allow your frontend's origin
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+    }),
+  );
+
   await app.listen(5000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
